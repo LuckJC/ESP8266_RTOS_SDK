49e8d2c119de8b2c04d6fe0ea80c186346ea772a
charles <958028483@qq.com>
2018-03-26 01:09:00 -0700

    report mac addr

--- examples/myproject/user/user_main.c
+++ examples/myproject/user/user_main.c
@@ -42,7 +42,7 @@
 #define DEVICE_TYPE 		"gh_9e2cff3dfa51" //wechat public number
 #define DEVICE_ID 			"122475" //model ID
 
-#define TEST_CONNECT_OK  "http://ui.iot.skadiseye.wang/chip/api/test/linkwified?gateway_sn=%s&time=%s"
+#define TEST_CONNECT_OK  "http://ui.iot.skadiseye.wang/chip/api/test/linkwified?gateway_sn=%d:%d:%d:%d:%d:%d&time=%d"
 
 #define DEFAULT_LAN_PORT 	12476
 
@@ -243,6 +243,9 @@ void scan_done(void *arg, STATUS status)
 void wifi_handle_event_cb(System_Event_t	*evt)
 {
     char *rst_buf;
+    uint8 sta_mac[6];
+    u32 rtc_time;
+    char req_buf[48];
 	printf("event %x\n", evt->event_id);
 
 	switch (evt->event_id) {
@@ -271,8 +274,14 @@ void wifi_handle_event_cb(System_Event_t	*evt)
 			IP2STR(&evt->event_info.got_ip.gw));
 		printf("\n");		
         //user_conn_init();
-        
-        rst_buf = http_get(TEST_CONNECT_OK);
+
+        wifi_get_macaddr(STATION_IF, sta_mac);
+        rtc_time = system_get_rtc_time();
+        sprintf(req_buf, TEST_CONNECT_OK, 
+            sta_mac[0], sta_mac[1], sta_mac[2], sta_mac[3], sta_mac[4], sta_mac[5], sta_mac[6],
+            rtc_time);
+        printf(req_buf);
+        rst_buf = http_get(req_buf);
         if(!rst_buf) {
             printf("%s\n", rst_buf);
             free(rst_buf);
